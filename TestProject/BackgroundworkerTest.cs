5544f68bb7312c71e2e7b5a274a8bbbbda296458
Manoj Kumar <manoj.kumar2@landisgyr.com>
2018-01-08 11:50:04 +0530

    first commit

---
+++ TestProject/BackgroundworkerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public class BackgroundworkerTest
+    {
+        BackgroundWorker bg1 = null;
+        bool g_bFlag = true;
+        public void Test()
+        {
+            bg1 = new BackgroundWorker();
+            bg1.DoWork += Bg1_DoWork;
+            bg1.RunWorkerCompleted += Bg1_RunWorkerCompleted;
+            bg1.RunWorkerAsync();
+            Console.ReadLine();
+            g_bFlag = false;
+        }
+
+        private void Bg1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Console.WriteLine("Background worker finished task");
+        }
+
+        private void Bg1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while(g_bFlag)
+            {
+                Console.WriteLine("background worker doing work");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
---
+++ TestProject/XmlDemo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace TestProject
+{
+    public class XmlDemo
+    {
+        private XmlDocument m_oXmlDoc = new XmlDocument();
+        private string c_sFilenameBIxml = "BI_LOG.xml";
+        
+        public void Test()
+        {
+            m_oXmlDoc.Load(c_sFilenameBIxml);
+            XmlNode node = m_oXmlDoc.SelectSingleNode("Log/EN/Legend");
+        }
+
+    }
+}
